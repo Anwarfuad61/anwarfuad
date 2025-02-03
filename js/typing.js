5a437c46c2473f3866a27e0f6c70f9e24a96b9fc
Anwarfuad61 <anwarfuad123456789@gmail.com>
2025-02-03 11:34:48 +0700

    first commit

---
+++ js/typing.js
@@ -0,0 +1,10 @@
+var typing = new Typed('#typing', {
+    strings: ['an IoT Developer', 'a Word Processing Expert', 'a Software Developer', 'a Spreadsheet Analyst'],
+    typeSpeed: 25,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true,
+    backDelay: 2000,
+
+});
+
